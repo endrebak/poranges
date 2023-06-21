0118bdaee0ac9fb496f8c53af11f154294d23f67
endre bakken stovner <endrebakkenstovner@endres-MacBook-Air.local>
2023-06-21 18:18:29 +0200

    Added join and overlap

---
+++ pyoframe/__init__.py
@@ -0,0 +1,65 @@
+from typing import Optional, Tuple
+
+import polars as pl
+import pyoframe
+
+
+@pl.api.register_dataframe_namespace("interval")
+class IntervalFrame:
+    def __init__(self, df: pl.DataFrame):
+        self._df = df
+
+    def join(
+            self,
+            other: pl.DataFrame,
+            on: Optional[Tuple[str, str]] = None,
+            right_on: Optional[Tuple[str, str]] = None,
+            left_on: Optional[Tuple[str, str]] = None,
+            suffix: str = "_right"
+    ):
+        starts, ends, starts_2, ends_2 = _get_interval_columns(on, right_on, left_on)
+
+        return pyoframe.ops.join(
+            self._df.lazy(),
+            other.lazy(),
+            starts=starts,
+            ends=ends,
+            starts_2=starts_2,
+            ends_2=ends_2,
+            suffix=suffix
+        )
+
+    def overlap(
+            self,
+            other: pl.DataFrame,
+            on: Optional[Tuple[str, str]] = None,
+            right_on: Optional[Tuple[str, str]] = None,
+            left_on: Optional[Tuple[str, str]] = None
+    ):
+        starts, ends, starts_2, ends_2 = _get_interval_columns(on, right_on, left_on)
+
+        return pyoframe.ops.overlap(
+            self._df.lazy(),
+            other.lazy(),
+            starts=starts,
+            ends=ends,
+            starts_2=starts_2,
+            ends_2=ends_2
+        )
+
+
+def _get_interval_columns(
+        on: Optional[Tuple[str, str]] = None,
+        right_on: Optional[Tuple[str, str]] = None,
+        left_on: Optional[Tuple[str, str]] = None,
+) -> Tuple[str, str, str, str]:
+    if on is None:
+        if right_on is None or left_on is None:
+            raise ValueError("Either `on` or `right_on` and `left_on` must be specified.")
+        starts, ends = left_on
+        starts_2, ends_2 = right_on
+    else:
+        starts, ends = on
+        starts_2, ends_2 = on
+
+    return starts, ends, starts_2, ends_2
--- tests/join/test_join.py
+++ tests/join/test_join.py
@@ -1,6 +1,16 @@
+from datetime import date
+
 import polars as pl
 
-from pyoframe.join import compute_masks, repeat_other, repeat_frame, apply_masks, add_lengths, find_starts_in_ends, join
+from pyoframe.ops import (
+    compute_masks,
+    repeat_other,
+    repeat_frame,
+    apply_masks,
+    add_lengths,
+    find_starts_in_ends,
+    join,
+)
 
 CHROMOSOME_PROPERTY = "chromosome"
 CHROMOSOME2_PROPERTY = "chromosome_2"
@@ -40,27 +50,35 @@ DF2_COLUMNS = df2.columns
 
 
 def chromosome_join(df, df2):
-    return df.lazy().sort("starts", ENDS_PROPERTY).select([pl.all().implode()]).join(
-        df2.lazy().sort("starts", ENDS_PROPERTY).select([pl.all().implode()]), how="cross", suffix="_2"
+    return (
+        df.lazy()
+        .sort("starts", ENDS_PROPERTY)
+        .select([pl.all().implode()])
+        .join(
+            df2.lazy().sort("starts", ENDS_PROPERTY).select([pl.all().implode()]),
+            how="cross",
+            suffix="_2",
+        )
     )
 
 
 g = chromosome_join(df, df2)
 
 SCHEMA = {
-    'chromosome': pl.List(pl.Utf8),
-    'starts': pl.List(pl.Int64),
-    'ends': pl.List(pl.Int64),
-    'starts_2': pl.List(pl.Int64),
-    'ends_2': pl.List(pl.Int64),
+    "chromosome": pl.List(pl.Utf8),
+    "starts": pl.List(pl.Int64),
+    "ends": pl.List(pl.Int64),
+    "starts_2": pl.List(pl.Int64),
+    "ends_2": pl.List(pl.Int64),
     "genes": pl.List(pl.Utf8),
-    'starts_2in1': pl.List(pl.UInt32),
-    'ends_2in1': pl.List(pl.UInt32),
-    'starts_1in2': pl.List(pl.UInt32),
-    'ends_1in2': pl.List(pl.UInt32)
+    "starts_2in1": pl.List(pl.UInt32),
+    "ends_2in1": pl.List(pl.UInt32),
+    "starts_1in2": pl.List(pl.UInt32),
+    "ends_1in2": pl.List(pl.UInt32),
 }
 
-expected_result_starts_in_ends = pl.DataFrame({
+expected_result_starts_in_ends = pl.DataFrame(
+    {
         CHROMOSOME_PROPERTY: [["chr1"] * 4],
         STARTS_PROPERTY: [[0, 5, 6, 8]],
         ENDS_PROPERTY: [[6, 7, 10, 9]],
@@ -72,7 +90,7 @@ expected_result_starts_in_ends = pl.DataFrame({
         STARTS_1IN2_PROPERTY: [[1, 1, 3]],
         ENDS_1IN2_PROPERTY: [[1, 3, 3]],
     },
-    schema=SCHEMA
+    schema=SCHEMA,
 )
 
 SCHEMA_2 = SCHEMA.copy()
@@ -93,9 +111,9 @@ expected_result_compute_masks = pl.DataFrame(
         STARTS_1IN2_PROPERTY: [[1, 1, 3]],
         ENDS_1IN2_PROPERTY: [[1, 3, 3]],
         MASK_1IN2_PROPERTY: [[False, True, False]],
-        MASK_2IN1_PROPERTY: [[True, True, True, False]]
+        MASK_2IN1_PROPERTY: [[True, True, True, False]],
     },
-    schema=SCHEMA_2
+    schema=SCHEMA_2,
 )
 
 expected_result_apply_masks = pl.DataFrame(
@@ -113,12 +131,16 @@ expected_result_apply_masks = pl.DataFrame(
         MASK_1IN2_PROPERTY: [[False, True, False]],
         MASK_2IN1_PROPERTY: [[True, True, True, False]],
     },
-    schema=SCHEMA_2
+    schema=SCHEMA_2,
 )
 
 
 def test_find_starts_in_ends():
-    result = g.with_columns(find_starts_in_ends(STARTS_PROPERTY, ENDS_PROPERTY, STARTS2_PROPERTY, ENDS2_PROPERTY)).collect()
+    result = g.with_columns(
+        find_starts_in_ends(
+            STARTS_PROPERTY, ENDS_PROPERTY, STARTS2_PROPERTY, ENDS2_PROPERTY
+        )
+    ).collect()
     with pl.Config() as cfg:
         cfg.set_tbl_cols(-1)
         # print(result)
@@ -128,16 +150,12 @@ def test_find_starts_in_ends():
 
 
 def test_compute_masks():
-    result = expected_result_starts_in_ends.with_columns(
-        compute_masks()
-    )
+    result = expected_result_starts_in_ends.with_columns(compute_masks())
     assert result.frame_equal(expected_result_compute_masks)
 
 
 def test_apply_masks():
-    result = expected_result_compute_masks.with_columns(
-        apply_masks()
-    )
+    result = expected_result_compute_masks.with_columns(apply_masks())
     with pl.Config() as cfg:
         cfg.set_tbl_cols(-1)
         # print(result)
@@ -145,9 +163,7 @@ def test_apply_masks():
 
 
 def test_add_lengths():
-    result = expected_result_apply_masks.with_columns(
-        add_lengths()
-    )
+    result = expected_result_apply_masks.with_columns(add_lengths())
     with pl.Config() as cfg:
         cfg.set_tbl_cols(-1)
         # print(list(result))
@@ -156,35 +172,115 @@ def test_add_lengths():
 
 
 def test_repeat_other():
-    res = expected_result_apply_masks.with_columns(
-        [pl.lit([[2, 1, 1]]).alias("diffs")]
-    ).lazy().select(repeat_other(["starts_2", "ends_2"], STARTS_2IN1_PROPERTY, "diffs"))
+    res = (
+        expected_result_apply_masks.with_columns([pl.lit([[2, 1, 1]]).alias("diffs")])
+        .lazy()
+        .select(repeat_other(["starts_2", "ends_2"], STARTS_2IN1_PROPERTY, "diffs"))
+    )
     # print(res.collect())
-    res.collect().frame_equal(pl.DataFrame(
-        {
-            STARTS2_PROPERTY: [1, 3, 6, 6],
-            ENDS2_PROPERTY: [2, 8, 7, 7]
-        }
-    ))
+    res.collect().frame_equal(
+        pl.DataFrame({STARTS2_PROPERTY: [1, 3, 6, 6], ENDS2_PROPERTY: [2, 8, 7, 7]})
+    )
 
 
 def test_repeat_frame():
     res = expected_result_apply_masks.select(
-        repeat_frame([STARTS_PROPERTY, ENDS_PROPERTY], MASK_2IN1_PROPERTY, STARTS_2IN1_PROPERTY, ENDS_2IN1_PROPERTY)
+        repeat_frame(
+            [STARTS_PROPERTY, ENDS_PROPERTY],
+            MASK_2IN1_PROPERTY,
+            STARTS_2IN1_PROPERTY,
+            ENDS_2IN1_PROPERTY,
+        )
     )
     with pl.Config() as cfg:
         cfg.set_tbl_cols(-1)
-        print("repeat", res)
+        # print("repeat", res)
     assert res.frame_equal(
-        pl.DataFrame(
-            {
-                STARTS_PROPERTY: [0, 0, 5, 6],
-                ENDS_PROPERTY: [6, 6, 7, 10]
-            }
-        )
+        pl.DataFrame({STARTS_PROPERTY: [0, 0, 5, 6], ENDS_PROPERTY: [6, 6, 7, 10]})
     )
 
 
 def test_join():
-    join(df.lazy(), df2.lazy(), "_2", STARTS_PROPERTY, ENDS_PROPERTY, STARTS_PROPERTY, ENDS_PROPERTY)
-    assert 1
+    res = join(
+        df.lazy(),
+        df2.lazy(),
+        "_2",
+        STARTS_PROPERTY,
+        ENDS_PROPERTY,
+        STARTS_PROPERTY,
+        ENDS_PROPERTY,
+    )
+    expected = pl.DataFrame(
+        [
+            pl.Series("chromosome", ['chr1', 'chr1', 'chr1', 'chr1', 'chr1', 'chr1'], dtype=pl.Utf8),
+            pl.Series("starts", [0, 0, 5, 6, 5, 6], dtype=pl.Int64),
+            pl.Series("ends", [6, 6, 7, 10, 7, 10], dtype=pl.Int64),
+            pl.Series("starts_2", [1, 3, 6, 6, 3, 3], dtype=pl.Int64),
+            pl.Series("ends_2", [2, 8, 7, 7, 8, 8], dtype=pl.Int64),
+            pl.Series("genes", ['c', 'b', 'a', 'a', 'b', 'b'], dtype=pl.Utf8),
+        ]
+    )
+
+    assert res.collect().frame_equal(expected)
+
+
+def test_time():
+    df_1 = pl.DataFrame(
+        {
+            "id": ["1", "3", "2"],
+            "start": [
+                date(2022, 1, 1),
+                date(2022, 5, 11),
+                date(2022, 3, 4),
+            ],
+            "end": [
+                date(2022, 2, 4),
+                date(2022, 5, 16),
+                date(2022, 3, 10),
+            ]
+        }
+    )
+    df_2 = pl.DataFrame(
+        {
+            "start": [
+                date(2021, 12, 31),
+                date(2025, 12, 31),
+            ],
+            "end": [
+                date(2022, 4, 1),
+                date(2025, 4, 1),
+            ]
+        }
+    )
+
+    expected = pl.DataFrame(
+        [
+            pl.Series("id", ['1', '2'], dtype=pl.Utf8),
+            pl.Series("start", [date(2022, 1, 1), date(2022, 3, 4)], dtype=pl.Date),
+            pl.Series("end", [date(2022, 2, 4), date(2022, 3, 10)], dtype=pl.Date),
+            pl.Series("start_whatevz", [date(2021, 12, 31), date(2021, 12, 31)], dtype=pl.Date),
+            pl.Series("end_whatevz", [date(2022, 4, 1), date(2022, 4, 1)], dtype=pl.Date),
+        ]
+    )
+
+    res = df_1.interval.join(df_2, on=("start", "end"), suffix="_whatevz")
+    assert res.collect().frame_equal(expected)
+
+
+def test_overlap():
+    res = df.interval.overlap(
+        df2.lazy(),
+        on=("starts", "ends")
+    )
+    print(res.collect())
+    # ┌────────────┬────────┬──────┐
+    # │ chromosome ┆ starts ┆ ends │
+    # │ ---        ┆ ---    ┆ ---  │
+    # │ str        ┆ i64    ┆ i64  │
+    # ╞════════════╪════════╪══════╡
+    # │ chr1       ┆ 0      ┆ 6    │
+    # │ chr1       ┆ 5      ┆ 7    │
+    # │ chr1       ┆ 6      ┆ 10   │
+    # └────────────┴────────┴──────┘
+
+    assert 0
